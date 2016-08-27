ffef7746f3204debfc875cc2939ab0a10b6c2413
Luchillo <luchillo17@gmail.com>
2016-08-27 18:45:13 -0500

    Test 1 with api key

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-var request = require('request') ;
-var sensorLib = require('node-dht-sensor');
+const request = require('request');
+const sensorLib = require('node-dht-sensor');
+
 // Read from sensor & send to Dweet.io
-var sensor = {
-  initialize: function () {
-      return sensorLib.initialize(11, 4);
-  },
-  read: function () {
+class SensorClass {
+  constructor(sensor, request) {
+    this.key = 'CX79NXA1V6VZMC06';
+    this.sensor = sensor;
+    this.request = request;
 
-      // Read from sensor
-	var readout = sensorLib.read();
-	var temperature = readout.temperature.toFixed(2);
-	var humidity = readout.humidity.toFixed(2);
-	console.log('Temperature: ' + temperature + 'C, ' +
-          'humidity: ' + humidity + '%');
-	if(temperature>29){
-		console.log('alarm');
-	}
- 
-	request
+    if (sensor.initialize(11, 4)) {
+      setInterval(() => this.read(), 1000);
+    } else {
+      console.error('Error initializing sensor');
+    }
   }
-};
 
-// Initialize DHT11 sensor
-if (sensor.initialize()) {
-      setInterval(function () {
-          sensor.read();
-      }, 1000);
-} else {
-    console.warn('Failed to initialize sensor');
+  read() {
+    // Read from sensor
+    var readout = this.sensor.read();
+    var temperature = readout.temperature.toFixed(2);
+    var humidity = readout.humidity.toFixed(2);
+    console.log('Temperature: ' + temperature + 'C, ' +
+      'humidity: ' + humidity + '%');
+    if (temperature > 29) {
+      console.log();
+    }
+
+    request({
+      uri: 'https://api.thingspeak.com/channels/149656',
+      method: 'PUT',
+      json: {
+        name: 'Antitheft Showcase',
+        api_key: this.key
+      }
+    }, (err, res, body) => {
+      console.log(err, res, body);
+    });
+  }
 }
+
+let sensor = new SensorClass(sensorLib, request);
+
+sensor.read()
