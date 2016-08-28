3d631d4516b393bc42cdc4e75869e0939efc59a3
Luchillo <luchillo17@gmail.com>
2016-08-27 20:23:54 -0500

    Config timeouts

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ThingSpeakClient = require('thingspeakclient');
-const client = new ThingSpeakClient();
+const client = new ThingSpeakClient({updateTimeout: 500});
 const sensorLib = require('node-dht-sensor');
 
 // Read from sensor & send to Dweet.io
@@ -17,7 +17,7 @@ class SensorClass {
     });
 
     if (sensor.initialize(11, 4)) {
-      setInterval(() => this.read(), 5000);
+      setInterval(() => this.read(), 1000);
     } else {
       console.error('Error initializing sensor');
     }
